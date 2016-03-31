413298278a8213af462b05d38bc219d6b6318a8c
smoqadam <phpro.ir@gmail.com>
2016-04-01 00:28:09 +0430

    fix minor bugs

--- src/Smoqadam/TelegramCli.php
+++ src/Smoqadam/TelegramCli.php
@@ -2,11 +2,11 @@
 
 namespace Smoqadam;
 
-class TelegramCli{
+class TelegramCli
+{
 
 
     private $f;
-
 
 
     /**
@@ -16,47 +16,50 @@ class TelegramCli{
     public function __construct($socket)
     {
         $this->f = stream_socket_client($socket);
-        if($this->f === false){
+        if ($this->f === false) {
             throw new TException('Connect to remote socket failed.');
         }
     }
 
     public function __call($command, $arguments)
     {
-        return $this->run($command,$arguments);
+        return $this->run($command, $arguments);
     }
 
-    private function run($command,$args = array())
+    private function run($command, $args = array())
     {
-        if(!empty($args)){
-            $args = implode(' ',$args);
-        }else{
+        if (!empty($args)) {
+            $args = implode(' ', $args);
+        } else {
             $args = '';
         }
 
-        $command = str_replace("\n",'',$command);
-        $command = $command.' '.$args;
+        $command = str_replace("\n", '', $command);
+        $command = $command . ' ' . $args;
 
-        fwrite($this->f,$command.PHP_EOL);
+        fwrite($this->f, $command . PHP_EOL);
         $fg = fgets($this->f);
-        if(strpos($fg,'ANSWER') !== false){
-            $bytes = intval(str_replace('ANSWER ','',$fg));
-            $bytes_read = 0;
-            $jsonString = '';
-            while ($bytes_read < $bytes){
-                $jsonString .= fread($this->f, $bytes - $bytes_read);
-                $bytes_read = strlen($jsonString);
+
+        if ($fg) {
+            if (strpos($fg, 'ANSWER') !== false) {
+                $bytes = intval(str_replace('ANSWER ', '', $fg));
+                $bytes_read = 0;
+                $jsonString = '';
+                while ($bytes_read < $bytes) {
+                    $jsonString .= fread($this->f, $bytes - $bytes_read);
+                    $bytes_read = strlen($jsonString);
+                }
+                $result = json_decode($jsonString, true);
+
+                if (isset($result['error'])) {
+                    throw new \Exception($result['error'], $result['error_code']);
+                }
+
+                return $result;
             }
-            $result = json_decode($jsonString,true);
 
-            if(isset($result['error'])){
-                throw new \Exception($result['error'],$result['error_code']);
-            }
-
-            return $result;
-
-        }else{
-            throw new \Exception('Something went wrong. '.$fg);
+        } else {
+            throw new \Exception('Something went wrong. ' . $fg);
         }
     }
 }

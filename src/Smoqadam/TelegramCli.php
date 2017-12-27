eadc29a91c7a49d51de91e5e50f8bdd1db990209
web-rider <lapenkoak@gmail.com>
2017-12-27 15:51:37 +0000

    More stability

--- src/Smoqadam/TelegramCli.php
+++ src/Smoqadam/TelegramCli.php
@@ -4,63 +4,80 @@ namespace Smoqadam;
 
 class TelegramCli
 {
+	private $f;
+	protected $_errorMessage = null;
+	protected $_errorCode = null;
 
+	/**
+	 * TelegramCli constructor.
+	 * @param $socket
+	 */
+	public function __construct($socket)
+	{
+		$this->f = stream_socket_client($socket);
+		if ($this->f === false) {
+			throw new \Exception('Connect to remote socket failed.');
+		}
+	}
 
-    private $f;
+	public function __call($command, $arguments)
+	{
+		return $this->run($command, $arguments);
+	}
 
+	private function run($command, $args = array())
+	{
+		if (!empty($args)) {
+			$args = implode(' ', $args);
+		} else {
+			$args = '';
+		}
 
-    /**
-     * TelegramCli constructor.
-     * @param $socket
-     */
-    public function __construct($socket)
-    {
-        $this->f = stream_socket_client($socket);
-        if ($this->f === false) {
-            throw new \Exception('Connect to remote socket failed.');
-        }
-    }
+		$command = $command . ' ' . $args;
+		fwrite($this->f, str_replace("\n", '\n', $command) . PHP_EOL);
+		$answer = fgets($this->f); //"ANSWER $bytes" or false if an error occurred
+		if (is_string($answer)) {
+			if (substr($answer, 0, 7) === 'ANSWER ') {
+				$bytes = ((int) substr($answer, 7)) + 1; //+1 because the json-return seems to miss one byte
 
-    public function __call($command, $arguments)
-    {
-        return $this->run($command, $arguments);
-    }
+				if ($bytes > 0) {
+					$bytesRead = 0;
+					$jsonString = '';
+					//Run fread() till we have all the bytes we want
+					//(as fread() can only read a maximum of 8192 bytes from a read-buffered stream at once)
+					do {
+						$jsonString .= fread($this->f, $bytes - $bytesRead);
+						$bytesRead = strlen($jsonString);
+					} while ($bytesRead < $bytes);
+					$json = json_decode($jsonString, true);
+					if (!isset($json->error)) {
+						//Reset error-message and error-code
+						$this->_errorMessage = null;
+						$this->_errorCode = null;
+						//For "status_online" and "status_offline"
+						if (isset($json->result) && $json->result === 'SUCCESS') {
+							return true;
+						}
+						//Return json-array
+						return $json;
+					} else {
+						$this->_errorMessage = $json->error;
+						$this->_errorCode = $json->error_code;
+					}
+				}
+			}
+		}
+		return false;
+	}
 
-    private function run($command, $args = array())
-    {
-        if (!empty($args)) {
-            $args = implode(' ', $args);
-        } else {
-            $args = '';
-        }
+	public function getErrorMessage()
+	{
+		return $this->_errorMessage;
+	}
 
-        $command = str_replace("\n", '', $command);
-        $command = $command . ' ' . $args;
-
-        fwrite($this->f, $command . PHP_EOL);
-        $fg = fgets($this->f);
-
-        if ($fg) {
-            if (strpos($fg, 'ANSWER') !== false) {
-                $bytes = intval(str_replace('ANSWER ', '', $fg));
-                $bytes_read = 0;
-                $jsonString = '';
-                while ($bytes_read < $bytes) {
-                    $jsonString .= fread($this->f, $bytes - $bytes_read);
-                    $bytes_read = strlen($jsonString);
-                }
-                $result = json_decode($jsonString, true);
-
-                if (isset($result['error'])) {
-                    throw new \Exception($result['error'], $result['error_code']);
-                }
-
-                return $result;
-            }
-
-        } else {
-            throw new \Exception('Something went wrong. ' . $fg);
-        }
-    }
+	public function getErrorCode()
+	{
+		return $this->_errorCode;
+	}
 }
 
